d39a91b67b1e6ce3868d17823ca6e7ca0e8773b1
Yasira-source <myasira2020@gmail.com>
2022-03-21 23:59:59 +0300

    fresh changes

---
+++ lib/components/coustom_bottom_nav_bar.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:educanapp/screens/home/home_screen.dart';
+import 'package:educanapp/screens/profile/profile_screen.dart';
+
+import '../constants.dart';
+import '../enums.dart';
+
+class CustomBottomNavBar extends StatelessWidget {
+  const CustomBottomNavBar({
+    Key key,
+     this.selectedMenu,
+  }) : super(key: key);
+
+  final MenuState selectedMenu;
+
+  @override
+  Widget build(BuildContext context) {
+    final Color inActiveIconColor = Color(0xFFB6B6B6);
+    return Container(
+      padding: EdgeInsets.symmetric(vertical: 14),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        boxShadow: [
+          BoxShadow(
+            offset: Offset(0, -15),
+            blurRadius: 20,
+            color: Color(0xFFDADADA).withOpacity(0.15),
+          ),
+        ],
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(40),
+          topRight: Radius.circular(40),
+        ),
+      ),
+      child: SafeArea(
+          top: false,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              
+                    IconButton(
+                icon: SvgPicture.asset(
+                  "assets/icons/Shop Icon.svg",
+                  color: MenuState.home == selectedMenu
+                      ? kPrimaryColor
+                      : inActiveIconColor,
+                ),
+                onPressed: () =>
+                    Navigator.pushNamed(context, HomeScreen.routeName),
+              ),
+            
+            
+            
+             
+              
+              IconButton(
+                icon: SvgPicture.asset(
+                  "assets/icons/User Icon.svg",
+                  color: MenuState.profile == selectedMenu
+                      ? kPrimaryColor
+                      : inActiveIconColor,
+                ),
+                onPressed: () =>
+                    Navigator.pushNamed(context, ProfileScreen.routeName),
+              ),
+               IconButton(
+                icon: Icon(Icons.help),
+                onPressed: () {},
+              ),
+              IconButton(
+                icon: Icon(Icons.share_rounded),
+               
+              
+                onPressed: () {},
+              ),
+            ],
+          )),
+    );
+  }
+}
